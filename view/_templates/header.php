c6b71ba469bb4d654e03a42f9bcbdc7dd3e0419f
sinise <sebastian@momentos.dk>
2015-02-06 03:29:06 -0800

    sdfsfd

--- view/_templates/header.php
+++ view/_templates/header.php
@@ -15,42 +15,24 @@
 
         <!-- navigation -->
         <ul class="navigation">
-            <li <?php if (View::checkForActiveController($filename, "index")) { echo ' class="active" '; } ?> >
-                <a href="<?php echo Config::get('URL'); ?>index/index">Index</a>
-            </li>
 
             <?php if (Session::userIsLoggedIn()) { ?>
                 <li <?php if (View::checkForActiveController($filename, "rpi")) { echo ' class="active" '; } ?> >
                     <a href="<?php echo Config::get('URL'); ?>rpi/index">Manage</a>
                 </li>
-                <li <?php if (View::checkForActiveController($filename, "note")) { echo ' class="active" '; } ?> >
-                    <a href="<?php echo Config::get('URL'); ?>note/index">My Notes</a>
-                </li>
 
                 <?php if (Session::get('user_account_type') == 3) { ?>
-                    <li <?php if (View::checkForActiveController($filename, "dashboard")) { echo ' class="active" '; } ?> >
-                        <a href="<?php echo Config::get('URL'); ?>dashboard/index">Dashboard</a>
+                    <li <?php if (View::checkForActiveController($filename, "overview")) { echo ' class="active" '; } ?> >
+                        <a href="<?php echo Config::get('URL'); ?>profile/index">Users</a>
                     </li>
-                    <li <?php if (View::checkForActiveController($filename, "note")) { echo ' class="active" '; } ?> >
-                        <a href="<?php echo Config::get('URL'); ?>note/index">My Notes</a>
+                    <li <?php if (View::checkForActiveControllerAndAction($filename, "login/register")) { echo ' class="active" '; } ?> >
+                        <a href="<?php echo Config::get('URL'); ?>login/register">Register</a>
                     </li>
-                <li <?php if (View::checkForActiveController($filename, "overview")) { echo ' class="active" '; } ?> >
-                    <a href="<?php echo Config::get('URL'); ?>profile/index">Profiles</a>
-                </li>
-
                 <?php } ?>
-
-
-
-
-
             <?php } else { ?>
                 <!-- for not logged in users -->
                 <li <?php if (View::checkForActiveControllerAndAction($filename, "login/index")) { echo ' class="active" '; } ?> >
                     <a href="<?php echo Config::get('URL'); ?>login/index">Login</a>
-                </li>
-                <li <?php if (View::checkForActiveControllerAndAction($filename, "login/register")) { echo ' class="active" '; } ?> >
-                    <a href="<?php echo Config::get('URL'); ?>login/register">Register</a>
                 </li>
             <?php } ?>
         </ul>
